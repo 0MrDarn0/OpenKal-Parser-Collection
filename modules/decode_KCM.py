dd572ad364203d81589294d4475ea42222fb11cf
OpenKal <openkal@users.noreply.github.com>
2018-01-24 23:18:06 +0100

    Refactored KCM decode script

--- modules/decode_KCM.py
+++ modules/decode_KCM.py
@@ -1,103 +1,85 @@
 #!/usr/bin/python3.5
 
 import sys
-import struct
-import json
+import utility
+import numpy as np
 
-from PIL import Image
+from struct import unpack
+from utility import ValidationError
+from utility import VersionError
 
-SIZE_1 = 256 * 256
-SIZE_2 = 257 * 257
 
-# Reads a KCM7 map and writes its sub-components to disk
-def main(path):
-    with open(path, 'rb') as stream:
-        # Parse
-        crc, map_number, \
-        x_coordinate,    \
-        y_coordinate,    \
-        _, _, _, _,      \
-        version = struct.unpack('<IIIIIIIII', stream.read(9 * 4))
+class KCMFile(object):
+    __slots__ = [
+        'x',
+        'y',
+        'alpha_ids',
+        'decal_ids',
+        'alpha_maps',
+        'decal_map',
+        'color_map',
+        'height_map',
+    ]
+
+    _SIZE_1 = 256
+    _SIZE_2 = 257
+
+    def parse(self, stream):
+        _, _,       \
+        self.x,     \
+        self.y,     \
+        _, _, _, _, \
+        version = unpack('<9I', stream.read(36))
 
         if version != 7:
-            sys.exit('Only KCM version 7 is supported; is: ' + str(version))
+            raise VersionError('KCM version %d is unsupported' % version)
 
-        alpha_info = struct.unpack('BBBBBBBB', stream.read(8))
-        decal_info = struct.unpack('BBBBBBBB', stream.read(8))
+        # Read ids, slice empty (0xFF) positions
+        self.alpha_ids = list(unpack('<8B', stream.read(8)))
+        self.decal_ids = list(unpack('<8B', stream.read(8)))
 
-        # Skip 1. alpha map index
+        self.alpha_ids.sort()
+        self.decal_ids.sort()
 
-        alphas = [None] * 7
-        for i, _ in enumerate(alphas):
-            if alpha_info[i + 1] != 0xFF:
-                alphas[i] = read_alpha_map(stream)
+        alpha_count = sum(i != 0xFF for i in self.alpha_ids)
+        decal_count = sum(i != 0xFF for i in self.decal_ids)
 
-        height = read_height_map(stream)
+        self.alpha_ids = self.alpha_ids[:alpha_count]
+        self.decal_ids = self.decal_ids[:decal_count]
 
-        color = read_color_map(stream)
-        decal = read_decal_map(stream)
+        self.alpha_maps = []
+        for _ in range(alpha_count):
+            self.alpha_maps.append(
+                    np.frombuffer(stream.read(KCMFile._SIZE_1 ** 2), np.uint8))
+
+        self.height_map = np.frombuffer(
+                stream.read(2 * KCMFile._SIZE_2 ** 2), np.uint16)
+
+        self.color_map = np.frombuffer(
+                stream.read(3 * KCMFile._SIZE_1 ** 2), (np.uint8, 3))
+
+        self.decal_map = np.frombuffer(
+                stream.read(KCMFile._SIZE_1 ** 2), np.uint8)
 
         # Verify
-        if len(stream.read(1)) != 0:
-            sys.exit('Unable to parse KCM, too many bytes')
+        if stream.read(1):
+            raise ValidationError('Too many bytes in KCM structure')
 
-        # Write
-        for i, _ in enumerate(alphas):
-            if alphas[i] != None:
-                write_png(alphas[i], path + ".aplha-" + str(i))
+        return self
 
-        write_png(height, path + ".height", mode = 'I')
+    def write(self, stream):
+        raise NotImplementedError
 
-        # Most applications cannot interpret 16-bit grayscale
-        # PNGs, which is why a raw format is exported as well
 
-        write_raw_height(height, path + ".height.raw")
+def main(path):
+    with open(path, 'rb') as stream:
+        try:
+            kcm = KCMFile().parse(stream)
 
-        write_png(color, path + ".color", mode = 'RGB')
-        write_png(decal, path + ".decal")
+        except (VersionError, ValidationError) as e:
+            print(str(e) + ' in ' + path)
 
-        json_data = {
-            'checksum'     : crc,
-            'coordinate_x' : x_coordinate,
-            'coordiante-y' : y_coordinate,
-            'map'          : map_number,
-            'maps_alpha'   : alpha_info,
-            'maps_decal'   : decal_info,
-            'version'      : version
-        }
 
-        with open(path + '.json', 'w') as output:
-            json.dump(json_data, output,\
-                    sort_keys=True, indent=4, separators=(',', ': '))
-
-def read_alpha_map(stream):
-    return [struct.unpack('<B', stream.read(1))[0] for _ in range(0, SIZE_1)]
-
-def read_decal_map(stream):
-    return [struct.unpack('<B', stream.read(1))[0] for _ in range(0, SIZE_1)]
-
-def read_height_map(stream):
-    return [struct.unpack('<H', stream.read(2))[0] for _ in range(0, SIZE_2)]
-
-def read_color_map(stream):
-    return [struct.unpack('BBB', stream.read(3)) for _ in range(0, SIZE_1)]
-
-def write_raw_height(raw, path):
-    with open(path, 'wb') as output:
-        for v in raw:
-            output.write(struct.pack('>H', v))
-
-def write_png(raw, path, mode = 'L'):
-    if len(raw) == SIZE_1:
-        size = (256, 256)
-    else:
-        size = (257, 257)
-
-    im = Image.new(mode, size)
-    im.putdata(raw)
-
-    im.transpose(Image.FLIP_TOP_BOTTOM).save(path + ".png", "PNG")
-
-# Usage: python decode_KCM.py .../n_xxx_yyy.kcm
+# Usage: python struct_kcm.py path; performs a parse check, nothing else
 if __name__ == '__main__':
     main(sys.argv[1])
